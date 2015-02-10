b218f362b6206a85e8cde30fa2242fdfb9a42401
Joshua <joshuaatteberry@gmail.com>
2015-02-09 16:23:35 -0800

    remove y array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 var pigLatin = function(words) {
+
   var vowels = ['a', 'e', 'i', 'o', 'u'];
-
   if (vowels.indexOf(words[0]) >= 0) {
-
     return words.concat('ay');
+  } else {
+    var counter = 0
+    while (vowels.indexOf(words[counter]) < 0) {
+      if
+      counter += 1
+    }
+    var beginning_consanant = words.slice(0, counter);
+    var remainder = words.slice(counter);
+    return remainder.concat(beginning_consanant + 'ay');
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,12 @@ describe('pigLatin', function() {
   it('will add ay to the end of a word starting with a vowel', function() {
     expect(pigLatin('animal')).to.equal('animalay');
   });
+
+  it('will remove the first consanant and at it to the end along with ay', function() {
+    expect(pigLatin('dog')).to.equal('ogday');
+  });
+
+  it('will remove the first two consanants and add them to the end with ay', function() {
+    expect(pigLatin('spatula')).to.equal('atulaspay');
+  });
 });
